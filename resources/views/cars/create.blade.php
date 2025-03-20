490905f98789563e0241efed46f2afa5c41ce638
Balics Gábor <balicsgabor@gmail.com>
2025-03-20 17:52:13 +0100

    cars api + views

--- resources/views/cars/create.blade.php
+++ resources/views/cars/create.blade.php
@@ -1,5 +1,38 @@
 @extends("layout")
 
 @section("content")
-    
-@end
+    <div class="row">
+        <div class="mx-auto col-4">
+            <form method="POST" action="{{ route('cars.create') }}">
+                @csrf()
+                <div class="form-group">
+                    <label>Márka</label>
+                    <input
+                        type="text"
+                        name="brand"
+                        class="form-control"
+                    >
+                </div>
+                <div class="form-group">
+                    <label>Model</label>
+                    <input 
+                        type="text"
+                        name="model" 
+                        class="form-control"
+                    >
+                </div>
+                <div class="form-group">
+                    <label>Gyártási év</label>
+                    <input 
+                        type="number"
+                        name="year" 
+                        class="form-control"
+                    >
+                </div>
+                <button type="submit" class="btn btn-success mt-3">
+                    Létrehozás
+                </button>
+            </form>
+        </div>
+    </div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -24,19 +24,49 @@ Route::group([
     "prefix" => "cars",
     "controller" => CarController::class
 ], function() {
+    Route::get("/", "list")->name("cars.list");
+    Route::get("/create", "createForm")
+        ->name("cars.createForm");
+    Route::get("/update/{car}", "updateForm")
+        ->name("cars.updateForm");
 
+    Route::post("/create", "create")->name("cars.create");
+    Route::post("/update/{car}", "update")
+        ->name("cars.update");
+    Route::delete("/delete/{car}", "delete")
+        ->name("cars.delete");
 });
 
 Route::group([
     "prefix" => "services",
     "controller" => ServiceController::class
 ], function() {
+    Route::get("/", "list")->name("services.list");
+    Route::get("/create", "createForm")
+        ->name("services.createForm");
+    Route::get("/update/{service}", "updateForm")
+        ->name("services.updateForm");
 
+    Route::post("/create", "create")->name("services.create");
+    Route::post("/update/{service}", "update")
+        ->name("services.update");
+    Route::delete("/delete/{service}", "delete")
+        ->name("services.delete");
 });
 
 Route::group([
     "prefix" => "mechanics",
     "controller" => MechanicController::class
 ], function() {
+    Route::get("/", "list")->name("mechanics.list");
+    Route::get("/create", "createForm")
+        ->name("mechanics.createForm");
+    Route::get("/update/{mechanic}", "updateForm")
+        ->name("mechanics.updateForm");
 
+    Route::post("/create", "create")->name("mechanics.create");
+    Route::post("/update/{mechanic}", "update")
+        ->name("mechanics.update");
+    Route::delete("/delete/{mechanic}", "delete")
+        ->name("mechanics.delete");
 });
